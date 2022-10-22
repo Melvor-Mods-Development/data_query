797bfca7abd65685ab90304abf316374a554a06e
mazunki <rolferen@gmail.com>
2022-10-22 03:06:19 +0200

    update url and easiery querying

--- bin/items.sh
+++ bin/items.sh
@@ -1,15 +1,35 @@
 #!/bin/sh
-DEMOGAME='https://test.melvoridle.com/dlcPrep/assets/data/melvorDemo.json' 
-BASEGAME='https://test.melvoridle.com/dlcPrep/assets/data/melvorFull.json' 
-TOTHGAME='https://test.melvoridle.com/dlcPrep/assets/data/melvorTotH.json' 
+#
+# A script to query items in the game.
+# Usage:
+#	items.sh [selection]
+#	selection:
+#		- "gear": all equippable items
+#		- slot: any specific slot, must be capitalized
+#
+MAIN_RELEASE='https://melvoridle.com/assets/data'
+TEST_RELEASE='https://test.melvoridle.com/dlcPrep/assets/data'
+BASEPATH="${MAIN_RELEASE}"
+
+BASEGAME="${BASEPATH:?}/melvorFull.json"
+DEMOGAME="${BASEPATH:?}/melvorDemo.json"
+TOTHGAME="${BASEPATH:?}/melvorTotH.json" 
 
 MATCH="${1?Provide slot pls}"
 if [ -z "$1" ]; then
 	QUERY='.data.items[]'
-elif [ "${1}" = "gear" ]; then
-	QUERY='.data.items[] | select(has("validSlots"))'
 else
-	QUERY='.data.items[] | select(has("validSlots")) | select(.validSlots[] == "'${MATCH?}'")'
+	case "${1}" in
+		gear)
+			QUERY='.data.items[] | select(has("validSlots"))'
+			;;
+		Passive|Cape)
+			QUERY='.data.items[] | select(has("validSlots")) | select(.validSlots[] == "'${MATCH?}'")'
+			;;
+		*)
+			QUERY='.data.items[] | select(.name == ".*'${MATCH?}'.*")'
+			;;
+	esac
 fi
 
 curl -Ss "${DEMOGAME:?}" | jq "${QUERY:?}"
