d8ca9ebc7e45f0447bdfd8c7d42c9aff2624e266
mazunki <rolferen@gmail.com>
2022-10-22 03:24:34 +0200

    monsters go brr

---
+++ bin/monsters.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+#
+# A script to query monsters in the game.
+# Usage:
+#	monsters.sh [monster name]
+#
+MAIN_RELEASE='https://melvoridle.com/assets/data'
+TEST_RELEASE='https://test.melvoridle.com/dlcPrep/assets/data'
+BASEPATH="${MAIN_RELEASE}"
+
+BASEGAME="${BASEPATH:?}/melvorFull.json"
+DEMOGAME="${BASEPATH:?}/melvorDemo.json"
+TOTHGAME="${BASEPATH:?}/melvorTotH.json" 
+
+MATCH="${1}"
+if [ -z "$MATCH" ]; then
+	QUERY='.data.monsters[]'
+else
+	case "${MATCH}" in
+		*)
+			QUERY='.data.monsters[] | select(.name | test(".*'${MATCH}'.*"))'
+			;;
+	esac
+fi
+
+curl -Ss "${DEMOGAME:?}" | jq "${QUERY:?}"
+curl -Ss "${BASEGAME:?}" | jq "${QUERY:?}"
+curl -Ss "${TOTHGAME:?}" | jq "${QUERY:?}"
+
