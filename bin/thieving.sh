b5d8034f2e4a01ca595cd8f6d32a0e21f31b1448
mazunki <rolferen@gmail.com>
2022-10-24 02:24:51 +0200

    support querying thieving

---
+++ bin/thieving.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+#
+# A script to query monsters in the game.
+# Usage:
+#	monsters.sh [monster name]
+#
+MAIN_RELEASE='https://melvoridle.com/assets/data'
+TEST_RELEASE='https://test.melvoridle.com/dlcPrep/assets/data'
+BASEPATH="${MAIN_RELEASE}"
+
+BASEGAME="${BASEPATH:?}/melvorFull.json"
+DEMOGAME="${BASEPATH:?}/melvorDemo.json"
+TOTHGAME="${BASEPATH:?}/melvorTotH.json" 
+
+MATCH="${1}"
+if [ -z "$MATCH" ]; then
+	QUERY='.data'
+else
+	case "${MATCH}" in
+		unique)
+			QUERY='.data.skillData[] | select(.skillID == "melvorD:Thieving") | .data.npcs[] | { name, uniqueDrop }'
+			;;
+		*)
+			QUERY='.data.skillData[] | select(.skillID == "melvorD:Thieving") | .data.npcs[] | select(.name | test(".*'${MATCH}'.*"))'
+			;;
+	esac
+fi
+
+curl -Ss "${DEMOGAME:?}" | jq "${QUERY:?}"
+curl -Ss "${BASEGAME:?}" | jq "${QUERY:?}"
+curl -Ss "${TOTHGAME:?}" | jq "${QUERY:?}"
+
